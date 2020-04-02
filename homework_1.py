361f85b71e038ea0582ac318e3c8a5ca41a63b66
Ilya Baiduzhiy <ilya.baiduzhiy@yandex.ru>
2020-04-02 10:27:48 +0300

    First homework completed

---
+++ homework_1.py
@@ -0,0 +1,9 @@
+a = 45
+print(a)
+
+b = "hello World!"
+print(b)
+
+name = input("Hello! What is your name: ")
+age = int(input("What is your age?: "))
+print(f"Now I know your name and age! And your name is {name}, age is {age}")
